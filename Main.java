3d3fe745a22dfae00d2d7240e89c0b2c8321ac11
sam <thambad@gmail.com>
2023-07-02 20:41:23 +0800

    added 2d array, yet to populate

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public ArrayList<String> daySequence(){
         System.out.println("How many days are there in the month?");
-        System.out.println("Which day does the 1st fall on?");
+        Scanner s = new Scanner(System.in);
+        int daysInMonth = s.nextInt();
+        //options of storing data: 2d array
+        String[][] calendar;
+        if(daysInMonth > 28){
+            calendar = new String[5][7];
+        } else {
+            calendar = new String[4][7];
+        }
 
+        System.out.println("Which day number does the 1st fall on? eg. Monday=0,Tuesday=1...");
+        int FirstDateDay = s.nextInt();
     }
 }
