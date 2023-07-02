4023c7df7593448bcf4b6b7e5fb05dba13382f78
sam <thambad@gmail.com>
2023-07-02 21:05:33 +0800

    hopefully populated calendar 2d array

--- Main.java
+++ Main.java
@@ -6,20 +6,30 @@ public class Main {
         System.out.println("How many days are there in the month?");
         Scanner s = new Scanner(System.in);
         int daysInMonth = s.nextInt();
+        ArrayList<Integer> dateList;
+        for(int i = 0; i< daysInMonth; i++){
+            dateList.add(i+1);
+        }
+
         //options of storing data: 2d array
-        String[][] calendar;
+        int[][] calendar;
         if(daysInMonth > 28){
-            calendar = new String[5][7];
+            calendar = new int[5][7];
         } else {
-            calendar = new String[4][7];
+            calendar = new int[4][7];
         }
 
         System.out.println("Which day number does the 1st fall on? eg. Monday=0,Tuesday=1...");
         int firstDateDay = s.nextInt();
         for(int i=0;i< calendar.length; i++){
             for(int j=0;j< calendar[0].length; j++){
-                if(i==0 && j==firstDateDay){
-                   //print from 1-30
+                if(i==0 && j>=firstDateDay) {
+                    //start from the firstDateDay
+                    //print from 1-30 inside the 2d list
+                    calendar[i][j] = dateList.remove(0);
+                } else if (i>0) {
+                    //after the first row of the calendar, fill as per normal
+                    calendar[i][j] = dateList.remove(0);
                 }
             }
         }
